49f14b1858d385d14fb717e716242dfb3807d652
lemmehoop <lemmehoop@gmail.com>
2022-05-06 14:13:54 +0300

    heap_sort left

--- src/merging.cpp
+++ src/merging.cpp
@@ -13,12 +13,19 @@ namespace assignment {
     int right_offset = middle + 1;
 
     // индекс текущей позиции буфера (туда будут сливаться подмассивы)
-    int buf_offset = -1 /* здесь что-то не так */;
+    int buf_offset = start /* здесь что-то не так */;
 
     // слияния подмассивов (пока не достигли конца одного из подмассивов)
     while (left_offset <= middle && right_offset <= stop) {
 
       // копируем элемент с наименьшим значением в буфер ...
+      if (arr[left_offset] <= arr[right_offset]) {
+        buf[buf_offset] = arr[left_offset];
+        left_offset += 1;
+      } else {
+        buf[buf_offset] = arr[right_offset];
+        right_offset += 1;
+      }
 
       // перемещаем текущую позицию вставки в буфер
       buf_offset += 1;
@@ -31,8 +38,15 @@ namespace assignment {
     }
 
     // реализуйте сливание остатков правого подмассива ...
+    for (/* ... */; right_offset <= stop; right_offset++) {
+      buf[buf_offset] = arr[right_offset];
+      buf_offset += 1;
+    }
 
     // копируем результат слияния подмассивов из буфера в оригинальный массив ... std::copy или цикл for ...
+    for (int i = start; i <= stop; i++) {
+      arr[i] = buf[i];
+    }
   }
 
 }  // namespace assignment
